31f418cf72d5c8c5bd8ddb6bdfe3d8ad5e34f705
Riko Pals <142883593+rkpls@users.noreply.github.com>
2023-10-12 14:08:46 +0200

    001

--- js.js
+++ js.js
@@ -7,3 +7,13 @@ function acceptCookies() {
     localStorage.setItem('cookiesAccepted', 'true');
     document.getElementById('cookie-banner').style.display = 'none';
 }
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
