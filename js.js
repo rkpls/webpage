12ae0db0348b5cae1bf6dc9213091bf8ee2efb6a
rkpls <142883593+rkpls@users.noreply.github.com>
2023-10-13 17:29:24 +0200

    001

--- js.js
+++ js.js
@@ -1,6 +1,21 @@
+function googleSearch() {
+    var cx = '014733843224484844763:tlxdtg2wgag';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  }
+
+function bingSearch() {
+    id="bcs_js_snippet"
+    src="https://ui.customsearch.ai/api/ux/rendering-js?customConfig=12bd9a66-1cb2-4d75-8d0e-a3d623dda59e&market=de-DE&version=latest&q="
+}
 
 if (!localStorage.getItem('cookiesAccepted')) {
-    document.getElementById('cookie-banner').style.display = 'block';
+    document.getlementById('cookie-banner').style.display = 'block';
 }
 
 function acceptCookies() {
