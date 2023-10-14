fefaef16b9b9ef97dbdbe515610b07c9d76ce8cd
Riko Pals <rikopals99@gmail.com>
2023-10-14 06:11:50 +0200

    001

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
+
+let googleSearchInput;
+let bingSearchInput;
+
 function googleSearch() {
-    var cx = '014733843224484844763:tlxdtg2wgag';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  }
+    let gSearch = 'https://www.google.com/search?q=' ;
+    googleSearchInput = null;
+}
 
 function bingSearch() {
-    id="bcs_js_snippet"
-    src="https://ui.customsearch.ai/api/ux/rendering-js?customConfig=12bd9a66-1cb2-4d75-8d0e-a3d623dda59e&market=de-DE&version=latest&q="
+    let bSearch = 'https://www.bing.com/search?q=' ;
+    bingSearchInput = null;
 }
+
 
 if (!localStorage.getItem('cookiesAccepted')) {
     document.getlementById('cookie-banner').style.display = 'block';
