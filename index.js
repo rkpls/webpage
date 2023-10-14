21d306b2aa45c7bf20e67a016a6ea1cac1346956
Riko Pals <rikopals99@gmail.com>
2023-10-14 06:13:01 +0200

    0001

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 
-let googleSearchInput;
-let bingSearchInput;
 
-function googleSearch() {
-    let gSearch = 'https://www.google.com/search?q=' ;
+function googleSearch(gSearch) {
+    getlementById.g_search()
+    let gSearchLink = 'https://www.google.com/search?q=' ;
     googleSearchInput = null;
 }
 
-function bingSearch() {
-    let bSearch = 'https://www.bing.com/search?q=' ;
+function bingSearch(bSearch) {
+    getElementById.b_search()
+    let bSearchLink = 'https://www.bing.com/search?q=' ;
     bingSearchInput = null;
 }
 
 
-if (!localStorage.getItem('cookiesAccepted')) {
-    document.getlementById('cookie-banner').style.display = 'block';
-}
 
 function acceptCookies() {
     localStorage.setItem('cookiesAccepted', 'true');
     document.getElementById('cookie-banner').style.display = 'hidden';
 }
+
+if (!localStorage.getItem('cookiesAccepted')) {
+    document.getlementById('cookie-banner').style.display = 'block';
+}
+
 
