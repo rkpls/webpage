28f2f4093119e0df5534bb1b3111d551728ac5cc
Riko Pals <rikopals99@gmail.com>
2023-10-18 17:41:37 +0200

    001

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleDarkMode(){
+    var elementBody = document.body;
+    var elementDiv = document.div;
+    elementBody.classList.toggle("dark-mode");
+    elementDiv.classList.toggle("dark-mode");
+}
