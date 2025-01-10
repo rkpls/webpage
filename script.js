7a4c85cbb7e5bbc2abefd9f15354ad48e6e59923
Riko Pals <rikopals99@gmail.com>
2025-01-10 11:13:16 +0100

    1

--- script.js
+++ script.js
@@ -84,18 +84,3 @@ setTimeout(() => {
     }, 1000);
   }
 }, 5000);
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const collapsibles = document.querySelectorAll(".collapsible");
-  collapsibles.forEach((header) => {
-      header.addEventListener("click", () => {
-          const content = header.nextElementSibling;
-          if (content.classList.contains("open")) {
-              content.classList.remove("open");
-          } else {
-              content.classList.add("open");
-          }
-      });
-  });
-});
